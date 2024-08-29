bddadf910e7faddb408f17b38682af93527faf72
Koen Handekyn <github.com@handekyn.com>
2024-08-29 12:51:41 +0200

    Update headers_and_footers.rb
    adjust to latest api

--- examples/headers_and_footers.rb
+++ examples/headers_and_footers.rb
@@ -25,25 +25,20 @@ HEADER_HTML = <<~HTML
 HTML
 
 Palapala.setup do |config|
-  config.debug = true
-  config.headless_chrome_url = 'http://localhost:9222' # run against a remote Chrome instance
+  # config.debug = true
+  # config.headless_chrome_url = 'http://localhost:9222' # run against a remote Chrome instance
   # config.headless_chrome_path = '/usr/bin/google-chrome-stable' # path to Chrome executable
 end
 
 result = Palapala::Pdf.new(
   # "<style>@page { size: A4 landscape; }</style><p>Hello world #{Time.now}</>",
   "<h1>Title</h1><p>Hello world #{Time.now}</>",
-  header_html: HEADER_HTML,
-  footer_html: '<div style="text-align: center;">Generated with Palapala PDF</div>',
+  header_template: HEADER_HTML,
+  footer_template: '<div style="text-align: center; font-size: 12pt; width: 100%;">Generated with Palapala PDF</div>',
   scale: 0.75,
   prefer_css_page_size: false,
-  margin: { top: 3, bottom: 2 }
-).save('tmp/headers_and_footers.pdf',
-       generateDocumentOutline: false,
-       #  marginTop: 1,
-       #  paperWidth: 3,
-       displayHeaderFooter: true,
-       #  landscape: false,
-       headerTemplate: HEADER_HTML)
+  margin_top: 3,
+  margin_bottom: 2).save('tmp/headers_and_footers.pdf')
 
 puts result
+`open tmp/headers_and_footers.pdf`
